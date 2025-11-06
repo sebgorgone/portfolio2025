f1d4ace7f091f49a6b0d66f4c6a62673037a6336
sebby-omarchy <sebgorgone@gmail.com>
2025-11-05 22:17:31 -0500

    title cards

--- src/Home.tsx
+++ src/Home.tsx
@@ -44,7 +44,7 @@ function Home() {
             style={{
               display: "flex",
               width: sideBar ? "300px" : "0px",
-              backgroundColor:colorMode ? "black" : "white",
+              backgroundColor: colorMode ? "black" : "white",
               minHeight: "calc(100vh - 128px)",
               overflow: "hidden",
               alignItems: "flex-end",
@@ -115,17 +115,17 @@ function Home() {
             <div
               style={{
                 display: "flex",
-                marginTop: "48px",
                 justifyContent: "center",
                 gap: "48px",
-                width: "100%"
+                width: "100%",
+                padding: "48px"
               }}
             >
               {/*card 1*/}
               <div
                 style={{
                   display: "flex",
-                  flexGrow: 1,
+                  flex: 1,
                   height: '512px',
                   backgroundColor: colorMode ? palDark[5] : "white",
                   maxWidth: "512px",
@@ -143,6 +143,9 @@ function Home() {
                     width: "100%"
                   }}
                 >
+
+
+
 
                   <div
                     style={{
@@ -177,9 +180,9 @@ function Home() {
                           className="hov"
                           src='svg/edits-icon.svg'
                           style={{
-                          width: "48px",
-                          height: "48px"
-                        }}
+                            width: "48px",
+                            height: "48px"
+                          }}
                         />
                       </button> :
                       <button
@@ -196,37 +199,465 @@ function Home() {
                           className="def"
                           src='svg/edits-icon.svg'
                           style={{
-                          width: "48px",
-                          height: "48px"
-                        }}
+                            width: "48px",
+                            height: "48px"
+                          }}
                         />
 
                         <img
                           className="hov"
                           src='svg/edits-icon-black.svg'
                           style={{
-                          width: "48px",
-                          height: "48px"
-                        }}
+                            width: "48px",
+                            height: "48px"
+                          }}
                         />
                       </button>}
 
 
-                      <h3
-                        style={{
-                          display: "flex",
-                          fontFamily: "subHeader",
-                          color: colorMode ? palDark[1] : palLight[5],
-                        }}
-                      >Video Editing</h3>
+                    <h3
+                      style={{
+                        display: "flex",
+                        fontFamily: "subHeader",
+                        color: colorMode ? palDark[1] : palLight[5],
+                      }}
+                    >Video Editing</h3>
 
 
                   </div>
 
 
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "center",
+                    }}
+                  >
+
+                    <h2
+                      style={{
+                        display: "flex",
+                        fontFamily: "title",
+                        color: colorMode ? palDark[0] : palLight[5],
+                        marginBottom: "8px",
+                        marginTop: "4px",
+                        textAlign: "center",
+                      }}
+                    >Collegiate Skydive Nationals</h2>
+
+                  </div>
+
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "center",
+                      flex: 1,
+                    }}
+                  >
+                    <video
+                      src="https://res.cloudinary.com/djuul0yr0/video/upload/v1720000000/Collegiates2024EditCOMPR_skypfr.mp4"
+                      autoPlay
+                      loop
+                      muted
+                      playsInline
+                      controls
+                      style={{
+                        width: "36%",
+                        objectFit: "cover",
+                        borderRadius: "12px",
+                        boxShadow: bs
+                      }}
+                    />
+
+                  </div>
+
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "space-between",
+                    }}
+                  >
+
+                    <p
+                      style={{
+                        display: "flex",
+                        fontFamily: "body",
+                        color: colorMode ? palDark[0] : palLight[5],
+                        marginTop: "8px",
+                      }}
+                    >Collegiates Z-Hills 2024</p>
+
+                  </div>
+
+
+
+
                 </div>
               </div>
 
+
+              {/* card 2 */}
+              <div
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  height: '512px',
+                  backgroundColor: colorMode ? palDark[5] : "white",
+                  maxWidth: "512px",
+                  minWidth: "380px",
+                  boxShadow: bs,
+                  borderTop: `solid 8px ${palLight[4]}`,
+                  padding: "8px"
+                }}
+              >
+
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
+                    flexGrow: 1,
+                  }}
+                >
+
+
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "space-between",
+                    }}
+                  >
+
+                    {colorMode ?
+                      <button
+                        className='navButton'
+                        type='button'
+                        onClick={() => { }}
+                        style={{
+                          width: "48px",
+                          height: "48px",
+                          marginLeft: "0px"
+                        }}
+                      >
+                        <img
+                          className="def"
+                          src='svg/design-icon-black.svg'
+                          style={{
+                            width: "48px",
+                            height: "48px"
+                          }}
+                        />
+
+                        <img
+                          className="hov"
+                          src='svg/design-icon.svg'
+                          style={{
+                            width: "48px",
+                            height: "48px"
+                          }}
+                        />
+                      </button> :
+                      <button
+                        className='navButton'
+                        type='button'
+                        onClick={() => { }}
+                        style={{
+                          width: "48px",
+                          height: "48px",
+                          marginLeft: "0px"
+                        }}
+                      >
+                        <img
+                          className="def"
+                          src='svg/design-icon.svg'
+                          style={{
+                            width: "48px",
+                            height: "48px"
+                          }}
+                        />
+
+                        <img
+                          className="hov"
+                          src='svg/design-icon-black.svg'
+                          style={{
+                            width: "48px",
+                            height: "48px"
+                          }}
+                        />
+                      </button>}
+
+
+                    <h3
+                      style={{
+                        display: "flex",
+                        fontFamily: "subHeader",
+                        color: colorMode ? palDark[1] : palLight[5],
+                      }}
+                    >Graphic Design</h3>
+
+
+                  </div>
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "center",
+                    }}
+                  >
+
+                    <h2
+                      style={{
+                        display: "flex",
+                        fontFamily: "title",
+                        color: colorMode ? palDark[0] : palLight[5],
+                        marginBottom: "8px",
+                        marginTop: "4px",
+                        textAlign: "center"
+                      }}
+                    >Adobe Illustrator Design</h2>
+
+                  </div>
+
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "center",
+                      flex: 1,
+                    }}
+                  >
+                    <img 
+                      src='designs/illustrator/imadethisinalecture.png'
+                      style={{width: "70%", boxShadow: bs}}
+                    />
+
+                  </div>
+
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "space-between",
+                    }}
+                  >
+
+                    <p
+                      style={{
+                        display: "flex",
+                        fontFamily: "body",
+                        color: colorMode ? palDark[0] : palLight[5],
+                        marginTop: "8px",
+                      }}
+                    >Ellington SkydiveCT 2023</p>
+
+                  </div>
+
+
+
+
+                </div>
+              </div>
+
+              {/* card 3 */}
+              <div
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  height: '512px',
+                  backgroundColor: colorMode ? palDark[5] : "white",
+                  maxWidth: "512px",
+                  minWidth: "380px",
+                  boxShadow: bs,
+                  borderTop: `solid 8px ${palLight[4]}`,
+                  padding: "8px"
+                }}
+              >
+
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%"
+                  }}
+                >
+
+
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "space-between",
+                    }}
+                  >
+
+                    {colorMode ?
+                      <button
+                        className='navButton'
+                        type='button'
+                        onClick={() => { }}
+                        style={{
+                          width: "48px",
+                          height: "48px",
+                          marginLeft: "0px"
+                        }}
+                      >
+                        <img
+                          className="def"
+                          src='svg/code-icon-black.svg'
+                          style={{
+                            width: "48px",
+                            height: "48px"
+                          }}
+                        />
+
+                        <img
+                          className="hov"
+                          src='svg/code-icon.svg'
+                          style={{
+                            width: "48px",
+                            height: "48px"
+                          }}
+                        />
+                      </button> :
+                      <button
+                        className='navButton'
+                        type='button'
+                        onClick={() => { }}
+                        style={{
+                          width: "48px",
+                          height: "48px",
+                          marginLeft: "0px"
+                        }}
+                      >
+                        <img
+                          className="def"
+                          src='svg/code-icon.svg'
+                          style={{
+                            width: "48px",
+                            height: "48px"
+                          }}
+                        />
+
+                        <img
+                          className="hov"
+                          src='svg/code-icon-black.svg'
+                          style={{
+                            width: "48px",
+                            height: "48px"
+                          }}
+                        />
+                      </button>}
+
+
+                    <h3
+                      style={{
+                        display: "flex",
+                        fontFamily: "subHeader",
+                        color: colorMode ? palDark[1] : palLight[5],
+                      }}
+                    >Programming</h3>
+
+
+                  </div>
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "center",
+                    }}
+                  >
+
+                    <h2
+                      style={{
+                        display: "flex",
+                        fontFamily: "title",
+                        color: colorMode ? palDark[0] : palLight[5],
+                        marginBottom: "8px",
+                        marginTop: "4px",
+                      }}
+                    >JS 'blockblast'</h2>
+
+                  </div>
+
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "center",
+                      flex: 1,
+                    }}
+                  >
+                    <iframe 
+                      style={{
+                        border: "none",
+                        boxShadow: bs,
+                        width: "70%",
+                        height: "200px",
+                      }}
+                      src='https://sebgorgone.github.io/js-blockblast/'
+                    />
+
+                  </div>
+
+
+
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      justifyContent: "space-between",
+                    }}
+                  >
+
+                    <p
+                      style={{
+                        display: "flex",
+                        fontFamily: "body",
+                        color: colorMode ? palDark[0] : palLight[5],
+                        marginTop: "8px",
+                      }}
+                    >Blockblast clone made with p5.js</p>
+
+                  </div>
+
+
+
+
+                </div>
+              </div>
 
 
             </div>
